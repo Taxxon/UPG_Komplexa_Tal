a06d34610f5cdf79c5a97cf57529063caf277844
Taxxon <te15.emil.kack@umea.nti.se>
2017-02-27 12:53:59 +0100

    Klar med allt.

--- src/Komplex.java
+++ src/Komplex.java
@@ -6,11 +6,13 @@ public class Komplex {
     double real;
     double imaginär;
 
+    //metod som gör om double till komplex
     public Komplex (double re, double im) {
         this.real = re;
         this.imaginär = im;
     }
 
+    // Plusar på två komplex
     public Komplex add(Komplex a){
         double newReal = this.real + a.real;
         double newImaginär = this.imaginär + a.imaginär;
@@ -19,6 +21,7 @@ public class Komplex {
         return svar;
     }
 
+    // multipliserar två komplex
     public Komplex multiply(Komplex a){
         double newReal = (this.real * a.real) - (this.imaginär * a.imaginär);
         double newImaginär = (this.imaginär * a.real) + (this.real * a.imaginär);
@@ -26,16 +29,24 @@ public class Komplex {
         return svar;
     }
 
+    // metod för att räkna ut vinkeln i radianer
     public double arg() {
        return (Math.atan2(this.imaginär, this.real));
     }
 
+    //metod för att titta om två komplex är samma
+    public boolean equals(Komplex a){
+        if(this.real == a.real && this.imaginär == a.imaginär){
+            return true;
+        } else {
+            return false;
+        }
 
+    }
 
+    //Skriver ut ett tal som (Re(Z)+Im(Z)i)
     public String toString() {
         return real + " + " + imaginär + "i";
     }
-
-
 
 }
--- src/Test.java
+++ src/Test.java
@@ -4,21 +4,33 @@
 public class Test {
     public static void main(String[] arg) {
 
+        //test för metoden add som funkar
         Komplex test = new Komplex(2,3);
         Komplex test2 = new Komplex(1,2);
         Komplex summa = test.add(test2);
 
         System.out.println(summa.toString());
 
-
+        //test för metoden multiply  som funkar
         Komplex test3 = new Komplex(2,3);
         Komplex test4 = new Komplex(1,2);
         Komplex summa2 = test.multiply(test2);
 
         System.out.println(summa2.toString());
 
+        //test för metoden arg som funkar
         double vinkel = test.arg();
 
         System.out.println("Vinkeln är " + vinkel);
+
+        //test för equals metoden som funkar
+        Komplex test5 = new Komplex(2,6);
+        Komplex test6 = new Komplex(2,6);
+
+        boolean same = test.equals(test2);
+        boolean same2 = test5.equals(test6);
+
+        System.out.println(same);
+        System.out.println(same2);
     }
 }
